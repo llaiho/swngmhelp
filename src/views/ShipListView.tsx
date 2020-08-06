adf1d88496ebedd7c5bc5e579f4a0b08ee34d898
unknown <llaiho666@gmail.com>
2020-08-06 19:50:09 +0300

    pre-alpha ship UI

--- src/views/ShipListView.tsx
+++ src/views/ShipListView.tsx
@@ -1,9 +1,10 @@
 import React, { FC, useState } from "react";
-import { makeStyles, Theme, createStyles, Container, Card, Button } from "@material-ui/core";
+import { makeStyles, Theme, createStyles, Container, Card, Button, Grid } from "@material-ui/core";
 
 import "./data-view.scss";
 import { randomShipGenerator } from "../generators/createShip";
-import { Ship } from "../interfaces/Ship";
+import { Ship, ShipFitting, ShipDefense, ShipWeapon } from "../interfaces/Ship";
+import { shipFittingsCostModifier } from "../data/ShipFittings";
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
@@ -29,6 +30,20 @@ const useStyles = makeStyles((theme: Theme) =>
             fontFamily: theme.typography.fontFamily,
             fontSize: "1.6rem",
             borderLeft: "solid 1rem rgba(255,255,255,0.5)",
+
+            borderTopLeftRadius: "1rem 1rem",
+            borderBottomLeftRadius: "1rem 1rem",
+            textShadow: "-1px -1px 1px white, 1px -1px 1px white, 1px 1px 1px white, -1px 1px 1px white",
+        },
+
+        part: {
+            margin: "2rem 0 0.5rem 0.5rem",
+            paddingLeft: "0.5rem",
+            position: "relative",
+            color: theme.palette.primary.dark,
+            fontFamily: theme.typography.fontFamily,
+            fontSize: "1.6rem",
+            // borderLeft: "solid 1rem rgba(255,255,255,0.5)",
 
             borderTopLeftRadius: "1rem 1rem",
             borderBottomLeftRadius: "1rem 1rem",
@@ -87,8 +102,10 @@ const useStyles = makeStyles((theme: Theme) =>
 );
 
  
-const ShipListView: FC = () => {
+export const ShipListView: FC = () => {
     const classes = useStyles();
+
+    const [selectedShip, setSelectedShip] = useState<Ship|null>(null);
 
     
     const [ships, setShips] = useState<Ship[]>([
@@ -113,15 +130,32 @@ const ShipListView: FC = () => {
             return newShips;
         });
     }  
-    
 
+    
+    // if (selectedShip !== null) {
+    //     return (
+    //         <Container>
+    //             <header className={classes.header}>
+    //                 <h2>testing</h2>
+    //             </header>
+    //         </Container>
+    //     );
+    // }
+
+    console.log("ship selected", selectedShip)
+    if (selectedShip !== null) {
+        return <ShipEditView ship={selectedShip} deselect={ () => setSelectedShip(null) } />
+        
+    }
+
+    
 
 
 
     return (
         <Container className="data-view">
             <header className={classes.header}>
-                <h2>Ship List list</h2>
+                <h2>Ship List</h2>
             </header>
         
         <h4 className = {classes.partHeader}>Ships</h4>
@@ -130,7 +164,7 @@ const ShipListView: FC = () => {
             {ships.map((ship) => {
                 return (
                     <div key={ship.id}>
-                        <p>{ship.shipName}, {ship.shipBaseHull.hullName} </p>
+                        <p onClick={() => setSelectedShip(ship)}>{ship.shipName}, {ship.shipBaseHull.hullName} </p>
                         
                         <Button onClick={() => [deleteShipFromList(ship.id)]}>Delete Ship</Button>
                     </div>    
@@ -152,4 +186,170 @@ const ShipListView: FC = () => {
     );
 };
 
+
+
 export default ShipListView;
+
+interface ShipEditViewProps {
+    ship: Ship;
+    deselect: () => void;
+}
+
+const ShipEditView: FC<ShipEditViewProps> = (props) => {
+    const classes = useStyles();
+
+    return <Container className="data-view">
+        <header className={classes.header}>
+            <h2>{props.ship.shipName}</h2>
+        </header>
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={5}>
+                    <h4 className={classes.partHeader}>Ship hull: {props.ship.shipBaseHull.hullName}</h4>
+
+                </Grid>
+
+                <Grid item xs={5}>
+                    <h4 className={classes.part}>Ship size class: {props.ship.shipSizeClass}</h4>
+
+                </Grid>
+            
+            
+            </Grid>
+
+
+        </Card>
+
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={2}>
+                    <h4 className={classes.part}>Speed: {props.ship.shipSpeed}</h4>
+
+                </Grid>
+
+                <Grid item xs={2}>
+                    <h4 className={classes.part}>Armor: {props.ship.shipArmor}</h4>
+
+                </Grid>
+
+                <Grid item xs={2}>
+                    <h4 className={classes.part}>AC: {props.ship.shipAC}</h4>
+
+                </Grid>            
+                <Grid item xs={2}>
+                    <h4 className={classes.part}>HP: {props.ship.shipCurrentHP}/{props.ship.shipMaxHP}</h4>
+
+                </Grid>
+
+            </Grid>
+
+
+        </Card>
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={5}>
+                    <h4 className={classes.part}>Crew min/max/current: {props.ship.shipMinCrew}/{props.ship.shipMaxCrew}/{props.ship.shipCurrentCrew}</h4>
+
+                </Grid>
+
+
+                <Grid item xs={2}>
+                    <h4 className={classes.part}>Spike Drive: {props.ship.shipSpikeDrive}</h4>
+
+                </Grid>            
+                <Grid item xs={2}>
+                    <h4 className={classes.part}>Spike Fuel: {props.ship.shipFuelForSpike}</h4>
+
+                </Grid>
+
+            </Grid>
+
+
+        </Card>
+
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={5}>
+                    <h4 className={classes.part}>Cargo Space: {props.ship.shipCargo} tons</h4>
+
+                </Grid>
+
+                <Grid item xs={5}>
+                    <h4 className={classes.part}>Hidden Cargo Space: {props.ship.shipSmuglerCargo} tons</h4>
+
+                </Grid>
+
+        
+
+
+            </Grid>
+
+
+        </Card>
+
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={3}>
+                    <h4 className={classes.part}>Power: {props.ship.shipBaseHull.hullPower}/{props.ship.shipFreePower} free</h4>
+
+                </Grid>
+
+                <Grid item xs={3}>
+                    <h4 className={classes.part}>Mass: {props.ship.shipBaseHull.hullMass}/{props.ship.shipFreeMass} free</h4>
+
+                </Grid>
+
+                <Grid item xs={3}>
+                    <h4 className={classes.part}>Hardpoints: {props.ship.shipBaseHull.hullHardpoints}/{props.ship.shipFreeHardpoints} free</h4>
+
+                </Grid>            
+
+
+            </Grid>
+
+
+        </Card>
+
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={1}>
+                    <h4 className={classes.partHeader}>Fittings: </h4>
+                </Grid>
+                <Grid item xs={10}>
+                    <h4 className={classes.part}> {props.ship.shipAddedFittings.map((fit:ShipFitting) => <p>{fit.fittingName}</p>)}</h4>
+                </Grid>
+            </Grid>
+        </Card>
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={1}>
+                    <h4 className={classes.partHeader}>Weapons: </h4>
+                </Grid>
+                <Grid item xs={10}>
+                    <h4 className={classes.part}> {props.ship.shipAddedWeapons.map((fit:ShipWeapon) => <p>{fit.weaponName}({fit.weaponDamage})</p>)}</h4>
+                </Grid>                
+            </Grid>
+        </Card>
+
+        <Card>
+            <Grid container direction="row" justify="space-around"  alignItems="center" > 
+                <Grid item xs={1}>
+                    <h4 className={classes.partHeader}>Defenses: </h4>
+                </Grid>
+                <Grid item xs={10}>
+                    <h4 className={classes.part}>{props.ship.shipAddedDefenses.map((fit:ShipDefense) => <p>{fit.defenseName}</p>)}</h4>
+                </Grid>                
+            </Grid>
+        </Card>
+
+        <Button variant="contained" onClick={props.deselect}>Back</Button>
+    </Container>;
+}
+
